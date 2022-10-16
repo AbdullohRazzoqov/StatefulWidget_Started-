364397ad2ca7e1484f1f2c9a7253faa0c52ca569
ShokhjakhonRazokov <razzoqovabdulloh033@gmail.com>
2022-10-16 15:52:26 -0400

    new example

---
+++ lib/screen/home.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Start"),
+      ),
+    );
+  }
+}
