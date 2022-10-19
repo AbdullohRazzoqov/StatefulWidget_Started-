01bd77d0cabb178d83b05ad29122b68a5f9b6e65
ShokhjakhonRazokov <razzoqovabdulloh033@gmail.com>
2022-10-18 20:19:38 -0400

    hi

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class MyHomePage extends StatelessWidget {
   MyHomePage({super.key});
   final _formKey = GlobalKey<FormState>();
+  final _nameController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,10 +19,18 @@ class MyHomePage extends StatelessWidget {
                 Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: TextFormField(
+                    //?controller => yozgan qiymatlarni saqlaydi
+                    controller: _nameController,
+                    //?keyboardType => klavturani qanday bo'lishi controll qilamiz
+                    keyboardType: TextInputType.number,
+                    //? parol yozish uchun
+                    obscureText: true,
                     decoration: const InputDecoration(
                       labelText: "Name",
                       hintText: "User",
-                      suffixIcon: Icon(Icons.person),
+                      suffixIcon: Icon(
+                        Icons.person,
+                      ),
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.all(Radius.circular(30)),
                       ),
